ff7e646782258724893236ffee51911b5b394d4d
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-07-18 22:07:41 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModelEx.Models
+{
+    public class Employee
+    {
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public int empsalary { get; set; }
+    }
+
+
+    public class EmployeeVM
+    {
+        public string salarycolor { get; set; }
+        public string empname { get; set; }
+        public int empsalary { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewModelEx.Models;
+
+namespace ViewModelEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Employee> emp = new List<Models.Employee>
+            {
+                new Employee{ empid=1, empname="yogesh", empsalary=2000},
+                new Employee{ empid=2, empname="Ranjit", empsalary=7000},
+                new Employee{ empid=3, empname="Bhupendra", empsalary=1000}
+            };
+            //return View("Index", emp);
+
+            List<EmployeeVM> empvmobj = new List<Models.EmployeeVM>();
+            foreach (Employee empobj in emp)
+            {
+                EmployeeVM vmobj = new Models.EmployeeVM();
+                vmobj.empname = empobj.empname;
+                vmobj.empsalary = empobj.empsalary;
+                if (empobj.empsalary > 2000)
+                {
+                    vmobj.salarycolor = "red";
+                }
+                else
+                {
+                    vmobj.salarycolor = "green";
+                }
+                empvmobj.Add(vmobj);
+            }
+             return View("VmView",empvmobj);
+        }
+    }
+}
